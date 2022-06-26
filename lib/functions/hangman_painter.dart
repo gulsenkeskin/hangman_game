295d6799b1de841b79503393146bbb0a48b7c42e
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-06-26 15:35:05 +0300

    Update hangman_painter.dart
    asılı ip

--- lib/functions/hangman_painter.dart
+++ lib/functions/hangman_painter.dart
@@ -4,13 +4,25 @@ class HangManPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint();
+
     paint.color = Colors.grey;
     paint.style = PaintingStyle.fill;
 
-    //dikdörgen çizme
+    _drawFrame(canvas, size, paint);
+    _drawNoose(canvas, size, paint);
+  }
+
+  _drawNoose(Canvas canvas, Size size, Paint paint) {
+    var nooseStart = Offset(size.width / 2, 0);
+    var nooseEnd = Offset(size.width / 2, size.height / 5);
+    paint.strokeWidth = 8.0;
+    canvas.drawLine(nooseStart, nooseEnd, paint);
+  }
+
+  _drawFrame(Canvas canvas, Size size, Paint paint) {
     canvas.drawRect(Rect.fromLTRB(0, size.height, 12, 0), paint);
 
-    canvas.drawRect(Rect.fromLTRB(0, 0, size.width, 12), paint);
+    canvas.drawRect(Rect.fromLTRB(0, 0, size.width / 2, 12), paint);
   }
 
   @override
