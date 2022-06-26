08eba3f25e8a97e0058427290db35ea74534c1da
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-06-26 19:03:49 +0300

    Update puzzle.dart

--- lib/screens/puzzle.dart
+++ lib/screens/puzzle.dart
@@ -11,8 +11,40 @@ class Puzzle extends StatefulWidget {
 }
 
 class _PuzzleState extends State<Puzzle> {
+  int _selectedBoxIndex = 0;
+
+  void _updateSelectedIndex(int i) {
+    setState(() {
+      _selectedBoxIndex = i;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        child: Wrap(
+            spacing: 8.0,
+            runSpacing: 8.0,
+            children: List.generate(widget.guessWord.length, (i){
+              return _buildSingleCharacterBox(i);
+            })
+        )
+    );
+  }
+
+  Widget _buildSingleCharacterBox(int index) {
+    return GestureDetector(
+      onTap: () {
+        _updateSelectedIndex(index);
+      },
+      child: Container(
+        height: 48.0,
+        width: 48.0,
+        decoration: BoxDecoration(
+            color: index == _selectedBoxIndex ? Colors.limeAccent : Colors.white,
+            borderRadius: BorderRadius.circular(4.0)
+        ),
+      ),
+    );
   }
 }
