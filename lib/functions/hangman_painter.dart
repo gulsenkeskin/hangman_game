b042e933992e6337c0a50cbb89503cefffdba123
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-06-26 15:40:32 +0300

    Update hangman_painter.dart
    baş, kol , bacak çizimleri

--- lib/functions/hangman_painter.dart
+++ lib/functions/hangman_painter.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HangManPainter extends CustomPainter {
+  double _headHeight = 32.0;
+
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint();
@@ -10,6 +12,13 @@ class HangManPainter extends CustomPainter {
 
     _drawFrame(canvas, size, paint);
     _drawNoose(canvas, size, paint);
+
+    _drawHead(canvas, size, paint);
+    _drawBody(canvas, size, paint);
+    _drawLeg(canvas, size, paint, Limb.left);
+    _drawLeg(canvas, size, paint, Limb.right);
+    _drawHand(canvas, size, paint, Limb.left);
+    _drawHand(canvas, size, paint, Limb.right);
   }
 
   _drawNoose(Canvas canvas, Size size, Paint paint) {
@@ -25,9 +34,57 @@ class HangManPainter extends CustomPainter {
     canvas.drawRect(Rect.fromLTRB(0, 0, size.width / 2, 12), paint);
   }
 
+  _drawHead(Canvas canvas, Size size, Paint paint) {
+    paint.color = Colors.yellow;
+    paint.style = PaintingStyle.fill;
+    var nooseEnd = Offset(size.width / 2, size.height / 5);
+    canvas.drawCircle(nooseEnd, _headHeight, paint);
+  }
+
+  _drawBody(Canvas canvas, Size size, Paint paint) {
+    paint.color = Colors.blueAccent;
+    var bodyStart = Offset(size.width / 2, size.height / 5 + _headHeight);
+    var bodyEnd = Offset(size.width / 2, bodyStart.dy + (3 * _headHeight));
+    paint.strokeWidth = 8.0;
+    canvas.drawRect(
+        Rect.fromCenter(
+            center: Offset(size.width / 2, (bodyStart.dy + bodyEnd.dy) / 2),
+            height: _headHeight * 3,
+            width: _headHeight * 2),
+        paint);
+  }
+
+  _drawLeg(Canvas canvas, Size size, Paint paint, Limb limb) {
+    paint.color = Colors.yellowAccent;
+    var bodyStart = Offset(size.width / 2, size.height / 5 + _headHeight);
+
+    var dxStart = limb == Limb.left ? size.width / 2 - 16 : size.width / 2 + 16;
+    var legStart = Offset(dxStart, bodyStart.dy + (3 * _headHeight));
+
+    var dxEnd = limb == Limb.left ? size.width / 2 - 32 : size.width / 2 + 32;
+    var legEnd = Offset(dxEnd, legStart.dy + (3 * _headHeight));
+    paint.strokeWidth = 8.0;
+    canvas.drawLine(legStart, legEnd, paint);
+  }
+
+  _drawHand(Canvas canvas, Size size, Paint paint, Limb limb) {
+    var bodyStart = Offset(size.width / 2, size.height / 5 + _headHeight);
+
+    var dxStart = limb == Limb.left ? size.width / 2 - 16 : size.width / 2 + 16;
+    var handStart = Offset(dxStart, bodyStart.dy);
+
+    var dx = limb == Limb.left ? size.width / 2 - 64 : size.width / 2 + 64;
+
+    var handEnd = Offset(dx, handStart.dy + (2.5 * _headHeight));
+    paint.strokeWidth = 8.0;
+    canvas.drawLine(handStart, handEnd, paint);
+  }
+
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     // we will come back to this later...
     return false;
   }
 }
+
+enum Limb { left, right }
