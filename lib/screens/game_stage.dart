723bcc77e9781062c3626da622aa73aef9ae9d49
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-06-26 15:32:38 +0300

    Update game_stage.dart
    sopa figuru eklendi

--- lib/screens/game_stage.dart
+++ lib/screens/game_stage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hangman_game/functions/hangman_painter.dart';
 
 class GameStage extends StatefulWidget {
   const GameStage({Key? key}) : super(key: key);
@@ -10,8 +11,34 @@ class GameStage extends StatefulWidget {
 class _GameStageState extends State<GameStage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("Hangman Game"),),
+    var mediaQd = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+          padding: EdgeInsets.all(24.0),
+          width: mediaQd.width,
+          height: mediaQd.height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 270,
+                height: mediaQd.height,
+                padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
+                child: CustomPaint(
+                  painter: HangManPainter(),
+                  size: Size(
+                    (270 - 24.0),
+                    (mediaQd.height - 24.0),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Center(child: Text("Hangman")),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
