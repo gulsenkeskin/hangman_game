c6680de0ee0f15821c05e2dd9e827b31efa334ad
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-06-26 22:04:57 +0300

    Update character_picker.dart

--- lib/widgets/character_picker.dart
+++ lib/widgets/character_picker.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hangman_game/bloc/game_stage_bloc.dart';
 
 class CharacterPicker extends StatefulWidget {
-  const CharacterPicker({Key? key}) : super(key: key);
+  final GameStageBloc gameStageBloc;
+
+  const CharacterPicker({Key? key, required this.gameStageBloc})
+      : super(key: key);
 
   @override
   State<CharacterPicker> createState() => _CharacterPickerState();
@@ -10,43 +14,51 @@ class CharacterPicker extends StatefulWidget {
 class _CharacterPickerState extends State<CharacterPicker> {
   var _alphabets = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
 
-  late List<String> _guessedLetters;
-
   @override
   void initState() {
     super.initState();
-    _guessedLetters = [];
   }
 
   @override
   Widget build(BuildContext context) {
     var alphabetArr = _alphabets.split(',');
-    return Container(
-        child: Wrap(
-            spacing: 8.0,
-            runSpacing: 8.0,
-            children: List.generate(alphabetArr.length, (i){
-              var letter = alphabetArr[i];
-              return _buildSingleCharacter(letter);
-            })
-        )
-    );
+
+    return StreamBuilder(
+        stream: widget.gameStageBloc.guessedCharacters,
+        builder: (BuildContext ctxt,
+            AsyncSnapshot<List<String>> guessedLettersSnap) {
+          if (!guessedLettersSnap.hasData) return CircularProgressIndicator();
+
+          return Container(
+              child: Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  children: List.generate(alphabetArr.length, (i) {
+                    var letter = alphabetArr[i];
+                    return _buildSingleCharacter(
+                        guessedLettersSnap.data!, letter);
+                  })));
+        });
   }
 
-  Widget _buildSingleCharacter(String letter) {
+  Widget _buildSingleCharacter(List<String> guessedLetters, String letter) {
     return GestureDetector(
       onTap: () {
-        setState(() {
-          _guessedLetters.add(letter);
-        });
+        if (!guessedLetters.contains(letter)) {
+          guessedLetters.add(letter);
+          widget.gameStageBloc.updateGuessedCharacter(guessedLetters);
+
+          if (widget.gameStageBloc.curGuessWord.value.indexOf(letter) < 0) {
+            widget.gameStageBloc.updateLostBodyParts();
+          }
+        }
       },
       child: Container(
-        width: 24.0,
-        height: 24.0,
+        width: 32.0,
+        height: 32.0,
         decoration: BoxDecoration(
-            color: _guessedLetters.contains(letter)? Colors.grey : Colors.white,
-            borderRadius: BorderRadius.circular(4.0)
-        ),
+            color: guessedLetters.contains(letter) ? Colors.grey : Colors.white,
+            borderRadius: BorderRadius.circular(4.0)),
         child: Center(child: Text(letter)),
       ),
     );
