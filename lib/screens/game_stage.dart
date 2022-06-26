32343a51b1e79ad402f17ef6a8c30345100963e6
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-06-26 22:04:55 +0300

    Update game_stage.dart

--- lib/screens/game_stage.dart
+++ lib/screens/game_stage.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:hangman_game/bloc/game_stage_bloc.dart';
 import 'package:hangman_game/functions/hangman_painter.dart';
 import 'package:hangman_game/widgets/character_picker.dart';
 import 'package:hangman_game/widgets/puzzle.dart';
 
-class GameStage extends StatefulWidget {
-  const GameStage({Key? key}) : super(key: key);
 
+
+class GameStage extends StatefulWidget {
   @override
-  State<GameStage> createState() => _GameStageState();
+  State<StatefulWidget> createState() {
+    return _GameStage();
+  }
 }
 
-class _GameStageState extends State<GameStage> {
+class _GameStage extends State<GameStage> {
+   late GameStageBloc _gameStageBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _gameStageBloc = GameStageBloc();
+  }
+
+  @override
+  void dispose() {
+    // _gameStageBloc?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     var mediaQd = MediaQuery.of(context).size;
@@ -32,7 +49,7 @@ class _GameStageState extends State<GameStage> {
                 height: mediaQd.height,
                 padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
                 child: CustomPaint(
-                  painter: HangManPainter(),
+                  painter: HangManPainter(_gameStageBloc),
                   size: Size(
                     (270 - 24.0),
                     (mediaQd.height - 24.0),
@@ -41,16 +58,119 @@ class _GameStageState extends State<GameStage> {
               ),
               Expanded(
                 child: Container(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CharacterPicker(),
-                    Puzzle(
-                      guessWord: "GULSEN",
-                    )
-                  ],
-                )),
+                    child: ValueListenableBuilder(
+                      valueListenable: _gameStageBloc.curGuessWord,
+                      builder: (BuildContext ctxt, String guessWord,  child) {
+                        if (guessWord == null || guessWord == '') {
+                          return Center(
+                              child: RaisedButton(
+                                child: Text('Start New Game'),
+                                onPressed: () {
+                                  _gameStageBloc.createNewGame();
+                                },
+                              ));
+                        }
+
+                        return ValueListenableBuilder(
+                            valueListenable: _gameStageBloc.curGameState,
+                            builder: (BuildContext ctxt, GameState gameState,
+                                 child) {
+                              if (gameState == GameState.succeded) {
+                                return Column(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text('Well done! You got the right answer.',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 24.0)),
+                                      RaisedButton(
+                                        child: Text('Start New Game'),
+                                        onPressed: () {
+                                          _gameStageBloc.createNewGame();
+                                        },
+                                      )
+                                    ]);
+                              }
+
+                              if (gameState == GameState.failed) {
+                                return Column(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text('Oops you failed!',
+                                          style: TextStyle(
+                                            // color: Colors.red,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 24.0)),
+                                      RichText(
+                                        text: TextSpan(children: [
+                                          TextSpan(
+                                              text: 'The correct word was: ',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black,
+                                                  fontSize: 16.0)),
+                                          TextSpan(
+                                              text:
+                                              _gameStageBloc.curGuessWord.value,
+                                              style: TextStyle(
+                                                // color: Colors.red,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 24.0))
+                                        ]),
+                                      ),
+                                      RaisedButton(
+                                        child: Text('Start New Game'),
+                                        onPressed: () {
+                                          _gameStageBloc.createNewGame();
+                                        },
+                                      )
+                                    ]);
+                              }
+
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Row(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Text(
+                                        'Guess the correct district...',
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16.0),
+                                      ),
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.restore,
+                                          color: Colors.white,
+                                          size: 24.0,
+                                        ),
+                                        onPressed: () {
+                                          _gameStageBloc.createNewGame();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                  CharacterPicker(
+                                    gameStageBloc: _gameStageBloc,
+                                  ),
+                                  Puzzle(
+                                    guessWord: guessWord,
+                                    gameStageBloc: _gameStageBloc,
+                                  )
+                                ],
+                              );
+                            });
+                      },
+                    )),
               )
             ],
           )),
