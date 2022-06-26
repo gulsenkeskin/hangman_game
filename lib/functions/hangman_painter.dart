d03e92e012dc2c9c753f534f983a2b1f7f3ab72f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-06-26 14:37:11 +0300

    Create hangman_painter.dart

---
+++ lib/functions/hangman_painter.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HangManPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.grey;
+    paint.style = PaintingStyle.fill;
+
+    canvas.drawRect(Rect.fromLTRB(0, size.height, 12, 0), paint);
+
+    canvas.drawRect(Rect.fromLTRB(0, 0, size.width, 12), paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // we will come back to this later...
+    return false;
+  }
+}
